938f702dbbb1c6b488c3bb5e570996f5ace23b05
MArebato79 <miguelrebatovalero@gmail.com>
2025-06-07 21:15:34 +0200

    ejercicio de calcular area de rectangulo y circulo con funciones y prompt

--- JavaScript/primero.js
+++ JavaScript/primero.js
@@ -4,7 +4,7 @@ let numero2=9;
 let edad=numero1+numero2;
 let estudios=true;
 
-console.log("Hola mi nombre es"+nombre+" tengo "+edad+" años y estudio "+(estudios?"Si":"No") );
+console.log("Hola mi nombre es "+nombre+" tengo "+edad+" años y estudio "+(estudios?"Si":"No") );
 
 let nota=1;
 
@@ -18,4 +18,25 @@ if(nota < "5"){
     console.log("Sobresaliente");
 }else{
     console.error("Valor no valido");
+}
+
+
+let entradaCalculo=prompt("Introduce la forma que quieras calcular").toString;
+
+if( entradaCalculo.toLocaleLowerCase == "rectangulo"){
+    let base=parseFloat(prompt("Introduce la base del rectangulo"));
+    let altura=parseFloat(prompt("Introduce la altura"));
+    console.log("El area del rectangulo es "+calcularAreaRectangulo(base,altura));
+} else if (entradaCalculo.toLocaleLowerCase == "circulo"){
+    let radio=parseFloat(prompt("Introduce el radio:"));
+    console.log("El area del circulo es "+calcularAreaCirculo(radio));
+}else{
+    console.log("Introduce una forma valida");
+}
+
+function calcularAreaCirculo(radio){
+    return Math.PI*(radio^2);
+}
+function calcularAreaRectangulo(base,altura){
+    return base*altura
 }
